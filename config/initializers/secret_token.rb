4284847426fc7c39da1980b396b67cbf2ad8f4b7
Matt Briggs <matt@mattbriggs.net>
2011-09-21 17:59:19 -0400

    rails!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KetchupTimer::Application.config.secret_token = '033139ac7404a66e764913cbe1cef119c5650c046e71a3fab03d0172add233da14db87bb326b5b95221d6b47c13d609ceba3ee0e8036f76198de4f56adb8fbe7'
