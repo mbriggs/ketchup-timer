f5a6a620b47074236cd6d28f62bc68c873caaa4d
Matt Briggs <matt@mattbriggs.net>
2011-09-22 00:11:09 -0400

    basic capification

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,36 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+$:.unshift File.expand_path("~/.rvm/lib")
+require 'rvm/capistrano'
+require 'bundler/capistrano'
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, "Ketchup Timer"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :domain, "timer.mattbriggs.net"
+set :user, "root"
+set :app_dir, "/webapps/timer.mattbriggs.net"
+set :rvm_ruby_string, '1.9.2'
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :scm, :git
+set :repository, "git@github.com:mbriggs/ketchup-timer.git"
+set :branch, "master"
+set :git_shallow_clone, 1
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+role :web, domain
+role :app, domain
+role :db, domain, :primary => true
+
+default_run_options[:pty] = true
+set :use_sudo, :false
+set :deploy_to, app_dir
+set :deploy_via, :export
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+after "deploy:symlink" do
+  run "cd #{current_path} && RAILS_ENV=production bundle exec rake assets:clean assets:precompile"
+end
