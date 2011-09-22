b66fa0d4f909b75119facaf76b66bc6a77b121b6
Matt Briggs <matt@mattbriggs.net>
2011-09-22 00:11:43 -0400

    yet more infrastructure

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
-// This is a manifest file that'll be compiled into including all the files listed below.
-// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
-// be included in the compiled file accessible from http://example.com/assets/application.js
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-//= require_tree .
+//= require lib/jquery
+//= require lib/underscore
+//= require lib/backbone
+//= require globals
+//= require task
+//= require interruption
+//= require app
